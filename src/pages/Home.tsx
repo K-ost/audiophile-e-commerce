8b887d9cf1a650c821f967c80952043a3b04e364
K-ost <kostya@iMac-Kostya.local>
2024-04-04 01:43:19 +0300

    commit 3.2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import Banner from "../components/Banner"
 import Bring from "../components/Bring"
 import Categories from "../components/Categories"
 
@@ -5,6 +6,8 @@ const Home: React.FC = () => {
   return (
     <div>
       <Categories />
+
+      <Banner />
 
       <Bring />
     </div>
