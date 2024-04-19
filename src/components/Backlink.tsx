0bf6d13e35bce31953d92c035349ec817e0da23f
K-ost <kostya@iMac-Kostya.local>
2024-04-19 22:56:05 +0300

    commit 2.1

--- src/components/Backlink.tsx
+++ src/components/Backlink.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom"
 import styled from "styled-components"
 
 // Styles
@@ -10,9 +11,18 @@ const LinkBox = styled.div`
 `
 
 const Backlink: React.FC = () => {
+  const navigate = useNavigate()
+
+  // backLinkFunc
+  const backLinkFunc = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault()
+    navigate(-1)
+  }
+  
+
   return (
     <LinkBox>
-      <a href="#">Go Back</a>
+      <a href="#" onClick={backLinkFunc}>Go Back</a>
     </LinkBox>
   )
 }
